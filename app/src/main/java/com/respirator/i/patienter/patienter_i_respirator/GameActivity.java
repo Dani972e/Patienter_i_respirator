c8972efd9fc716a98c9045a45df7e287eba9b879
dani972e <anusic@live.dk>
2017-10-22 19:54:27 +0200

    Adding pictures design XML in different activities writing code

---
+++ app/src/main/java/com/respirator/i/patienter/patienter_i_respirator/GameActivity.java
@@ -0,0 +1,32 @@
+package com.respirator.i.patienter.patienter_i_respirator;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+public class GameActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private ImageButton home_btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+
+        ImageButton home_btn = (ImageButton) findViewById(R.id.home_btn);
+        home_btn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.home_btn:
+                Intent home = new Intent(this, MainActivity.class);
+                startActivity(home);
+                break;
+        }
+
+    }
+}
