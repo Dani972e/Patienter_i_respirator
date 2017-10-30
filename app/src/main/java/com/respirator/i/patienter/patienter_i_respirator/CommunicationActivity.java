32e1f389e65f7a73f237f67ae15c1a391c348465
dani972e <anusic@live.dk>
2017-10-30 12:56:58 +0100

    working on layout

--- app/src/main/java/com/respirator/i/patienter/patienter_i_respirator/CommunicationActivity.java
+++ app/src/main/java/com/respirator/i/patienter/patienter_i_respirator/CommunicationActivity.java
@@ -2,12 +2,42 @@ package com.respirator.i.patienter.patienter_i_respirator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
-public class CommunicationActivity extends AppCompatActivity {
+public class CommunicationActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private ImageView answer_btn;
+    private ImageView i_btn;
+    private ImageView iWantTo_btn;
+    private ImageView wsQuestion_btn;
+    private ImageView pain_btn;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_communication);
+
+        ImageView answer_btn = (ImageView) findViewById(R.id.answer_btn);
+        ImageView i_btn = (ImageView) findViewById(R.id.i_btn);
+        ImageView iWantTo_btn = (ImageView) findViewById(R.id.iWantTo_btn);
+        ImageView wsQuestion_btn = (ImageView) findViewById(R.id.wsQuestions_btn);
+        ImageView pain_btn = (ImageView) findViewById(R.id.pain_btn);
+
+
+        answer_btn.setOnClickListener(this);
+        i_btn.setOnClickListener(this);
+        iWantTo_btn.setOnClickListener(this);
+        wsQuestion_btn.setOnClickListener(this);
+        pain_btn.setOnClickListener(this);
+
+
+    }
+
+    @Override
+    public void onClick(View view) {
+
+
     }
 }
