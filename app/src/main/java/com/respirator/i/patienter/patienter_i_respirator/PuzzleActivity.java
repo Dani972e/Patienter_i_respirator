2905da98466cd6b49e7470c457e976102b7682d4
dani972e <anusic@live.dk>
2017-10-31 14:37:23 +0100

    Adding more activities

---
+++ app/src/main/java/com/respirator/i/patienter/patienter_i_respirator/PuzzleActivity.java
@@ -0,0 +1,13 @@
+package com.respirator.i.patienter.patienter_i_respirator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PuzzleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_puzzle);
+    }
+}
