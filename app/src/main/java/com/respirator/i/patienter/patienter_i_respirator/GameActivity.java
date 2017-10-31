a89f1eade0a07d88e00f0a4e2d581043d13d4789
dani972e <anusic@live.dk>
2017-10-31 14:50:30 +0100

    Adding intents and more activities

--- app/src/main/java/com/respirator/i/patienter/patienter_i_respirator/GameActivity.java
+++ app/src/main/java/com/respirator/i/patienter/patienter_i_respirator/GameActivity.java
@@ -9,23 +9,46 @@ import android.widget.ImageView;
 
 public class GameActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private ImageView home_btn;
+    private ImageView balloon_btn;
+    private ImageView memory_game_btn;
+    private ImageView ticTacToe_btn;
+    private ImageView puzzle_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
 
-        ImageView home_btn = (ImageView) findViewById(R.id.home_btn);
-        home_btn.setOnClickListener(this);
+        ImageView balloon_btn = (ImageView) findViewById(R.id.balloon_btn);
+        ImageView memory_game_btn = (ImageView) findViewById(R.id.memoryGame_btn);
+        ImageView ticTacToe_btn = (ImageView) findViewById(R.id.ticTacToe_btn);
+        ImageView puzzle_btn = (ImageView) findViewById(R.id.puzzle_btn);
+
+
+        balloon_btn.setOnClickListener(this);
+        memory_game_btn.setOnClickListener(this);
+        ticTacToe_btn.setOnClickListener(this);
+        puzzle_btn.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View view) {
         switch (view.getId()) {
-            case R.id.home_btn:
-                Intent home = new Intent(this, MainActivity.class);
-                startActivity(home);
+            case R.id.balloon_btn:
+                Intent balloonGame = new Intent(this, PopBalloonsActivity.class);
+                startActivity(balloonGame);
+                break;
+            case R.id.memoryGame_btn:
+                Intent memoryGame = new Intent(this, MemoryGameActivity.class);
+                startActivity(memoryGame);
+                break;
+            case R.id.ticTacToe_btn:
+                Intent ticTacToeGame = new Intent(this, TicTacToeActivity.class);
+                startActivity(ticTacToeGame);
+                break;
+            case R.id.puzzle_btn:
+                Intent puzzleGame = new Intent(this, PuzzleActivity.class);
+                startActivity(puzzleGame);
                 break;
         }
 
