6854e618b532d69b2afaaaefd483bdb222c7726b
dani972e <anusic@live.dk>
2017-11-15 12:40:00 +0100

    Changing style theme

---
+++ app/src/main/java/com/respirator/i/patienter/patienter_i_respirator/fragments/InputFragment.java
@@ -0,0 +1,24 @@
+package com.respirator.i.patienter.patienter_i_respirator.fragments;
+
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+
+public class InputFragment extends Fragment implements View.OnClickListener {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return null;
+    }
+
+    @Override
+    public void onClick(View view) {
+
+    }
+}
