469251632f6e481c603fcc0a91240d531d064a80
Thomas <tklorentzen@gmail.com>
2017-11-27 11:50:06 +0100

    Merge branch 'development' of C:\Users\Thomas\AndroidStudioProjects\Patienter_i_respiratorer with conflicts.

--- app/src/main/java/com/respirator/i/patienter/patienter_i_respirator/activities/CommActivity.java
+++ app/src/main/java/com/respirator/i/patienter/patienter_i_respirator/activities/CommActivity.java
@@ -13,6 +13,8 @@ import com.respirator.i.patienter.patienter_i_respirator.fragments.IWantToFragme
 import com.respirator.i.patienter.patienter_i_respirator.fragments.MyAnswerFragment;
 import com.respirator.i.patienter.patienter_i_respirator.fragments.WsQuestionsFragment;
 
+import static com.respirator.i.patienter.patienter_i_respirator.R.drawable;
+
 public class CommActivity extends AppCompatActivity implements View.OnClickListener {
 
     private ImageView answer_btn, i_btn, iWantTo_btn, wsQuestion_btn, pain_btn, call_btn;
