701d8fb61d0ead0b8721eb368c430fef38f4aff1
dani972e <anusic@live.dk>
2017-11-27 13:42:23 +0100

    Fragments in settings

--- app/src/main/java/com/respirator/i/patienter/patienter_i_respirator/activities/SettingsActivity.java
+++ app/src/main/java/com/respirator/i/patienter/patienter_i_respirator/activities/SettingsActivity.java
@@ -4,12 +4,15 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.respirator.i.patienter.patienter_i_respirator.R;
+import com.respirator.i.patienter.patienter_i_respirator.fragments.InputmetodeFragment;
 
 public class SettingsActivity extends AppCompatActivity implements View.OnClickListener {
 
     private ImageView indput_btn, sound_btn, fontsize_btn, language_btn, reset_btn;
+    private TextView settings_view;
 
 
     @Override
@@ -22,6 +25,7 @@ public class SettingsActivity extends AppCompatActivity implements View.OnClickL
         fontsize_btn = findViewById(R.id.fontsize_btn);
         language_btn = findViewById(R.id.language_btn);
         reset_btn = findViewById(R.id.reset_btn);
+        settings_view = findViewById(R.id.settings_view);
 
         indput_btn.setOnClickListener(this);
         sound_btn.setOnClickListener(this);
@@ -35,6 +39,9 @@ public class SettingsActivity extends AppCompatActivity implements View.OnClickL
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.input_btn:
+                getFragmentManager().beginTransaction().replace(R.id.settingsFragmentContainer, new InputmetodeFragment()).commit();
+                settings_view.setVisibility(View.INVISIBLE);
+                break;
 
         }
 
