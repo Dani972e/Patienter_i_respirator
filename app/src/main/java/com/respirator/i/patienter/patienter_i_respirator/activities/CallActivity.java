49f3ac84a7038efb5b1facf4857724969fab260b
dani972e <anusic@live.dk>
2017-11-30 13:23:42 +0100

    on click for settings

--- app/src/main/java/com/respirator/i/patienter/patienter_i_respirator/activities/CallActivity.java
+++ app/src/main/java/com/respirator/i/patienter/patienter_i_respirator/activities/CallActivity.java
@@ -2,7 +2,6 @@ package com.respirator.i.patienter.patienter_i_respirator.activities;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
 
 import com.respirator.i.patienter.patienter_i_respirator.R;
 
@@ -17,16 +16,5 @@ public class CallActivity extends AppCompatActivity {
 
     }
 
-    public void onWindowFocusChanged(boolean hasFocus) {
-        if (hasFocus) {
-            getWindow().getDecorView().setSystemUiVisibility(
-                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                            | View.SYSTEM_UI_FLAG_FULLSCREEN
-                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
-        }
 
-    }
 }
--- app/src/main/java/com/respirator/i/patienter/patienter_i_respirator/activities/SettingsActivity.java
+++ app/src/main/java/com/respirator/i/patienter/patienter_i_respirator/activities/SettingsActivity.java
@@ -70,15 +70,5 @@ public class SettingsActivity extends AppCompatActivity implements View.OnClickL
 
     }
 
-    public void onWindowFocusChanged(boolean hasFocus) {
-        if (hasFocus) {
-            getWindow().getDecorView().setSystemUiVisibility(
-                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                            | View.SYSTEM_UI_FLAG_FULLSCREEN
-                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
-        }
-    }
+
 }
