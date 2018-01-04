7884800f32bbb245509f1eb305fb6905ad5b64ee
ChristopherChytraeus <Christopherchytraeus@gmail.com>
2018-01-04 15:58:01 +0100

    Start på Huskespil

--- app/src/main/java/com/respirator/i/patienter/patienter_i_respirator/fragments/MemoryGameFragment.java
+++ app/src/main/java/com/respirator/i/patienter/patienter_i_respirator/fragments/MemoryGameFragment.java
@@ -7,12 +7,19 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import android.widget.ImageView;
+
 import com.respirator.i.patienter.patienter_i_respirator.R;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class MemoryGameFragment extends Fragment {
+public class MemoryGameFragment extends Fragment implements View.OnClickListener {
+
+    private final int btn_amount = 4;
+    private final ImageView[] btnArray = new ImageView[btn_amount];
+    private final int[] btnId = {R.id.imageMemory1, R.id.imageMemory2, R.id.imageMemory3, R.id.imageMemory4};
+
 
 
     public MemoryGameFragment() {
@@ -21,10 +28,31 @@ public class MemoryGameFragment extends Fragment {
 
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.memory_game_fragment, container, false);
+        View memory = inflater.inflate(R.layout.memory_game_fragment, container, false);
+
+
+        for (int i = 0; i < btnId.length; i++) {
+            btnArray[i] = memory.findViewById(btnId[i]);
+            btnArray[i].setOnClickListener(this);
+
+        }
+
+        return memory;
+    }
+
+
+    public void onClick (View v) {
+
+        for (ImageView btn : btnArray) {
+            if (btn.equals(v)) {
+                btn.setImageResource(R.mipmap.memory_image_moon);
+
+            }
+        }
     }
 
 }
