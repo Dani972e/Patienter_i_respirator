9b0b3023e32a0c110c89b3d03c830d4801457cdf
Johnny <johnnychen.jiahao@gmail.com>
2018-01-04 19:24:27 +0100

    Volume controls are added and proper icon is added

--- app/src/main/java/com/respirator/i/patienter/patienter_i_respirator/activities/CallActivity.java
+++ app/src/main/java/com/respirator/i/patienter/patienter_i_respirator/activities/CallActivity.java
@@ -1,9 +1,11 @@
 package com.respirator.i.patienter.patienter_i_respirator.activities;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 import com.respirator.i.patienter.patienter_i_respirator.R;
@@ -11,21 +13,38 @@ import com.respirator.i.patienter.patienter_i_respirator.R;
 public class CallActivity extends AppCompatActivity implements View.OnClickListener {
 
     private ImageView back_btn, home_btn;
+
+    private Button help_btn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         onWindowFocusChanged(true);
+
         setContentView(R.layout.call_activity);
 
         back_btn = findViewById(R.id.back_btn);
         home_btn = findViewById(R.id.home_btn);
+        help_btn = findViewById(R.id.help_btn);
 
         back_btn.setOnClickListener(this);
         home_btn.setOnClickListener(this);
+        help_btn.setOnClickListener(this);
+
+        final MediaPlayer help_sound = MediaPlayer.create(this,R.raw.multimedia_event_tone_1);
+
+        help_btn.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                help_sound.start();
+            }
+        });
     }
 
     @Override
     public void onClick(View view) {
+
         switch (view.getId()) {
             case R.id.back_btn:
                 onBackPressed();
--- app/src/main/java/com/respirator/i/patienter/patienter_i_respirator/fragments/SoundFragment.java
+++ app/src/main/java/com/respirator/i/patienter/patienter_i_respirator/fragments/SoundFragment.java
@@ -1,30 +1,52 @@
 package com.respirator.i.patienter.patienter_i_respirator.fragments;
 
 
-import android.os.Bundle;
 import android.app.Fragment;
+import android.content.Context;
+import android.media.AudioManager;
+import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.respirator.i.patienter.patienter_i_respirator.R;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class SoundFragment extends Fragment {
+public class SoundFragment extends Fragment implements View.OnClickListener {
 
+    private Button sound_up, sound_down;
 
-    public SoundFragment() {
-        // Required empty public constructor
-    }
+    public AudioManager audioManager;
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.sound_fragment, container, false);
+
+        View sound = inflater.inflate(R.layout.sound_fragment, container, false);
+
+        sound_up = sound.findViewById(R.id.sound_up);
+        sound_down = sound.findViewById(R.id.sound_down);
+
+        sound_up.setOnClickListener(this);
+        sound_down.setOnClickListener(this);
+
+        audioManager = (AudioManager) getActivity().getSystemService(Context.AUDIO_SERVICE);
+
+        return sound;
     }
 
+    @Override
+    public void onClick(View view) {
+        if (sound_up == view) {
+            audioManager.adjustStreamVolume(AudioManager.STREAM_MUSIC, AudioManager.ADJUST_RAISE, AudioManager.FLAG_SHOW_UI);
+        }
+        else if (sound_down == view) {
+            audioManager.adjustStreamVolume(AudioManager.STREAM_MUSIC, AudioManager.ADJUST_LOWER, AudioManager.FLAG_SHOW_UI);
+        }
+    }
 }
