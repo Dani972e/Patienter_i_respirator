2135614e962fefadfc9d8b91c2032fa3132366c3
Thomas <tklorentzen@gmail.com>
2018-01-16 11:18:18 +0100

    Ændret nogle spørgsmål

--- app/src/main/java/com/respirator/i/patienter/patienter_i_respirator/fragments/quizSpørgsmål.java
+++ app/src/main/java/com/respirator/i/patienter/patienter_i_respirator/fragments/quizSpørgsmål.java
@@ -6,18 +6,18 @@ package com.respirator.i.patienter.patienter_i_respirator.fragments;
 
 public class quizSpørgsmål {
 
-    public String[] minespørgsmål = {"Hvor mange mennesker bor i Danmark?", "Hvad er Danmarks hovedstad?", "Hvilket to lande grænser op til Danmark?",
-            "Hvor mange stater er der i USA?", "Hvad er 10 gange 5?", "Hvilke planeter ligger tættere på Solen end Jorden?",
-            "Hvor mange planeter er der i Solsystemet?", "Hvilket dyr nedstammer hunde fra?"};
+    public String[] minespørgsmål = {"Hvor mange mennesker bor i Danmark?", "Hvad er Danmarks hovedstad?", "Hvilket land grænser op til det sydlige Danmark?",
+            "Hvad hedder det danske flag?", "Hvad er 10 gange 5?", "Hvilket sprog taler de i Australien?",
+            "Hvad er Danmarks største ø?", "Hvilket dyr nedstammer hunde fra?"};
 
     private String minesvar[][] = {
             {"ca. 2 millioner", "ca. 5.5 millioner", "ca. 10 millioner", "ca. 7.5 millioner"}, {"Odense", "Århus", "København", "Esbjerg"},
-            {"Tyskland og Sverige", "Norge og Finland", "Storbritanien og Sverige", "Tyskland og Norge"},
-            {"17", "20", "50", "35"}, {"25", "50", "500", "150"}, {"Jupiter og Mars", "Merkur og Venus", "Venus og Saturn", "Merkur og Neptun"},
-            {"8", "12", "5", "10"}, {"Hyæner", "Bjørne", "Ræve", "Ulve"}
+            {"Tyskland", "Norge", "Sverige", "Polen"},
+            {"Højbrog", "Dronningebrog", "Dannebrog", "Nørrebrog"}, {"25", "50", "500", "150"}, {"Fransk", "Engelsk", "Australsk", "Spansk"},
+            {"Sjælland", "Fyn", "Bornholm", "Langeland"}, {"Hyæner", "Bjørne", "Løver", "Ulve"}
     };
 
-    public String korrektesvar[] = {"ca. 5.5 millioner", "København", "Tyskland og Sverige", "50", "50", "Merkur og Venus", "8", "Ulve"};
+    public String korrektesvar[] = {"ca. 5.5 millioner", "København", "Tyskland", "Dannebrog", "50", "Engelsk", "Sjælland", "Ulve"};
 
     public String getminespørgsmål(int a){
         String spørgsmål = minespørgsmål[a];
@@ -25,7 +25,6 @@ public class quizSpørgsmål {
     }
 
     public String getsvar1(int a){
-        System.out.println("svar: " + a + " længde: "+minesvar.length + " "+minesvar[a].length);
         String valg = minesvar[a][0];
         return valg;
     }
